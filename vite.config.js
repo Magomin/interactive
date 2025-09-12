e09e6293281e62e39cec56b86ba07c637c921e61
Magomin <matthieu@fribl.co>
2025-09-12 18:33:44 +0200

    update site

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,10 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  base: "/interactive/",
   plugins: [react()],
-});
+  base: '/interactive/',    // très important (nom de ton repo GitHub)
+  build: {
+    outDir: 'docs'          // 🔑 génère les fichiers buildés dans /docs
+  }
+})
